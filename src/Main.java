d62821b3d912b6bfec260bd61f4879ba6aa66dd9
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-21 14:54:32 +0530

    added changes

--- src/Main.java
+++ src/Main.java
@@ -55,6 +55,15 @@ public class Main {
         return flag;
     }
 
+    public static void EvenOrOdd() {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+        if (num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
 
     public static void main(String[] args)
     {
